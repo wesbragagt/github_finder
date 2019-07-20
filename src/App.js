727ec51e22021c7fbdad8caab367bfb1e4b17fe6
Weslley Braga <wesbragagt@gmail.com>
2019-07-20 00:21:37 -0500

    adding users with api call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Layout/Navbar';
-import UserItem from './Components/Users/UserItem';
+import Users from './Components/Users/Users';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-    
-    
+    state = {
+        users: [],
+        loading: false
+    };
+
+    async componentDidMount() {
+        this.setState({
+            loading: true
+        });
+        const res = await axios.get('https://api.github.com/users');
+        this.setState({
+            users: res.data,
+            loading: false
+        });
+    }
     render() {
-       
-       
         return (
             <div className="App">
                 <Navbar />
-                <UserItem/>
+                <div className="container">
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
+                </div>
             </div>
         );
     }
--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,27 +1,27 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-export class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    };
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-    render() {
-        return (
-            <Fragment>
-                <nav className="navbar bg-primary">
-                    <h1>
-                        <i className={this.props.icon} />
-                        {this.props.title}
-                    </h1>
-                </nav>
-            </Fragment>
-        );
-    }
-}
+const Navbar = ({ title, icon }) => {
+    return (
+        <Fragment>
+            <nav className="navbar bg-primary">
+                <h1>
+                    <i className={icon} />
+                    {title}
+                </h1>
+            </nav>
+        </Fragment>
+    );
+};
+
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
